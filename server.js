3af318ee3a6d52e0f46f49a2d3827c79c1de1d86
jose vega moreno <jvegamoreno@outlook.com>
2018-10-16 12:30:56 +0200

    change2

--- server.js
+++ server.js
@@ -17,6 +17,29 @@ app.get('/', (req, res) => {
     res.render('home', { customer })
 });
 
+app.get('/list', (req, res) => {
+    let customer = {
+        name: 'pepe'
+    }
+
+    let p1 = {
+        imgUrl: '/assets/img/burger.jpg',
+        name: 'cheese burger',
+        description: 'Muy apetitosa cheese burguer',
+        price: 7.50
+    }
+    let p2 = {
+        imgUrl: '/assets/img/burger.jpg',
+        name: 'Homemade Burrito',
+        description: 'Mexico knocks on your door. Will you let him in?',
+        price: 6
+    }
+
+    let products = [p1, p2, p2, p1, p1]
+
+    res.render('catalogue', { customer, products })
+})
+
 app.get('/about', (req, res) => {
     let customer = {
         name: 'pepe',
@@ -31,4 +54,10 @@ app.get('/about', (req, res) => {
 });
 
 
-app.listen(port);
+//app.listen(port, () => console.log('Running'));
+
+const fs = require('fs')
+
+fs.readFile("pepe.json", (err) => {
+    console.log('pepe leido')
+})
